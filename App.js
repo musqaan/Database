bd6be1434cc1719719013eea5aa1f1d5fe2493eb
musqaan <111404112+musqaan@users.noreply.github.com>
2025-02-17 17:22:29 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import EmployeeList from './EmployeeList';
+import TaskList from './TaskList';
 
-function App() {
-  return (
-    <div className="App">
-      <EmployeeList />
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <TaskList />
+        </div>
+    );
+};
 
 export default App;
