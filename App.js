f385a39bbabceb847e3006a19b5a9ac1704604de
musqaan <111404112+musqaan@users.noreply.github.com>
2025-02-18 17:21:19 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import TaskList from './TaskList';
+import { Routes, Route } from 'react-router-dom';
+ // Ensure About component exists
+import Register from './components/Register';  // Ensure Register component exists
+import Login from './components/Login';  // Ensure Login component exists
+import Dashboard from './components/Dashboard';  // Ensure Dashboard component exists
 
-const App = () => {
-    return (
-        <div className="App">
-            <TaskList />
-        </div>
-    );
-};
+function App() {
+  return (
+    <div>
+      <Routes>
+        
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
